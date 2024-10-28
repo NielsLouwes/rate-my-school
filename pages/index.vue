de83b06a2f038426534908e92f4eb780ffeb484b
Niels Louwes <nielslouwes45@gmail.com>
2024-10-28 16:53:54 +0100

    cleaned school data for use

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="text-4xl font-bold text-center p-10">RATE MY SCHOOL</div>
+
+  <div v-for="school in data" :key="school.uniqueId">
+    {{ school.schoolname }}
+  </div>
 </template>
 
 <script lang="ts" setup>
-import schools from "~/data/schools.json";
-console.log("schools", schools);
+import { filteredData } from "~/utils/data-clean";
+const data = filteredData();
 </script>
---
+++ utils/data-clean.ts
@@ -0,0 +1,16 @@
+import schools from "~/data/schools.json";
+
+export const filteredData = () =>
+  schools.map((school) => {
+    return {
+      schoolname: school.INSTELLINGSNAAM,
+      schooltype: school["SOORT HO"],
+      province: school.PROVINCIE,
+      streetName: school.STRAATNAAM,
+      houseNumber: school["HUISNUMMER-TOEVOEGING"],
+      postalCode: school.POSTCODE,
+      city: school.PLAATSNAAM,
+      website: school.INTERNETADRES,
+      uniqueId: school["BEVOEGD GEZAG NUMMER"],
+    };
+  });
